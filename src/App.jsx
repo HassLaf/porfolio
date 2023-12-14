b2a0971d5968bdc8dd843f0ce112e4e428f4176f
HassLaf <114194678+HassLaf@users.noreply.github.com>
2023-12-14 08:55:12 +0000

    Some changes in the site

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Hassan LF",
+  title: "Software Engineer Student | Full Stack Developer",
+  email: "hassan.lafai@gmail.com",
+  gitHub: "HassLaf",
+  instagram: "",
+  linkedIn: "hassan-lafai",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
